6b322107d52f7f580196f296d2822de7ecda5887
aditya8927 <adityasingh18112006@gmail.com>
2025-10-30 19:22:36 +0530

    Add files via upload

---
+++ java aditya.js
@@ -0,0 +1,27 @@
+student_name = "aditya"
+student_age = 19 
+student_marks = 99  
+if (student_marks >= 95) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'A+'","#Awesome")
+}
+else if (student_marks >= 90) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'A'","#Great")
+}
+else if (student_marks >= 80) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'B'","#Good")
+}
+else if (student_marks >= 70) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'C'","#Needs more practice")
+}
+else if (student_marks >= 60) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'D'","#2X your efforts")
+}
+else if (student_marks >= 50) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'E'","#marks doesn't matter !!")
+}
+else if (student_marks >= 40) {
+    console.log(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'F'","#boost your efforts,")
+}
+else {
+    console.log(student_name, ", Age:",student_age,",marks:",student_marks ,", FAILED","#demoted !")
+}
